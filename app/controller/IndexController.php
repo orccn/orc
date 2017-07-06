1c75231645b2e958acfb8691a79dc2d938d9b1dd
orccn <lpywy728394@163.com>
2017-07-06 18:26:18 +0800

    di

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -2,11 +2,18 @@
 namespace Controller;
 
 use orc\Controller;
+use orc\DI;
 
 class IndexController extends Controller
 {
     function index()
     {
+        $di = new DI();
+        $di->set('test', 'orc\Config');
+        $di->get('test',22);
+        exit;
+        
         return $this->fetch();
+        
     }
 }
